5572a0735bec2f913329b6f8826d9babf56e5fae
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-05 09:57:09 +0100

    Updated operation

--- operation.py
+++ operation.py
@@ -4,20 +4,19 @@ class Operation:
         self.__nombre2 = nombre2
         
     def get_nombre1(self):
-        return "Le nombre1 est", self.__nombre1
+        return f"Le nombre1 est {self.__nombre1}"
     
     def set_nombre1(self, nombre):
         self.__nombre1 = nombre
         
     def get_nombre2(self):
-        return "Le nombre2 est", self.__nombre2
+        return f"Le nombre2 est {self.__nombre2}"
     
     def set_nombre2(self, nombre):
         self.__nombre2 = nombre
         
     def addition(self):
         return self.__nombre1 + self.__nombre2
-        
         
 operation = Operation(12, 3)
 print(operation.get_nombre1())
