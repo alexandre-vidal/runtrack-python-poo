9b61b77d548497db29b7b6ee569da25d2482de00
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-05 11:21:18 +0100

    wip

---
+++ animal.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, prenom, age):
+        self.prenom= prenom
+        self.age = age
+        
+    def afficherNom(self):
+        return f"L'animal se nomme {self.prenom}"
+    
+    def afficherAge(self):
+        return f"L'animal a {self.age} ans"
+    
+    def vieillir(self):
+        return "L'animal a", int(self.age) + 1, "ans"
+    
+animal = Animal(input("Entrez le nom de l'animal : "), input("Entrez l'âge de l'animal : "))
+print(animal.afficherNom())
+print(animal.afficherAge())
+print(animal.vieillir())
